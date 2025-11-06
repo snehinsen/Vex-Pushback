0ec8de9f4dcc68030b44059d821c47655b3abca9
snehinsen <snehinsen@gmail.com>
2025-11-06 14:48:03 -0500

    feat: add Test.v5python

--- src/handlers.py
+++ src/handlers.py
@@ -3,10 +3,12 @@ from vex import *
 class Handlers:
     controller: Controller
     brain: Brain
+    driveTraint: DriveTrain
 
-    def __init__(self, b: Brain, c: Controller) -> None:
+    def __init__(self, b: Brain, c: Controller, d: DriveTrain) -> None:
         self.brain = b
         self.controller = c
+        self.driveTraint = d
 
     def test(self):
         self.controller.screen.print("Testing")
